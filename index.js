8fd476cbb16fb5d2eb417d48c5635a4f71f5bcf1
Sabieena <sabina.bhattrai2015@gmail.com>
2021-03-03 15:50:14 +0545

    use of multiple reducer

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const redux = require('redux')
 
 const createStore = redux.createStore
+const combineReducers = redux.combineReducers
 
 
 const BUY_CAKE = 'BUY_CAKE'
@@ -12,12 +13,19 @@ function buyCake() {
     }
 }
 
-const initialState = {
+function buyIceCream() {
+    return {
+        type: 'BUY_ICECREAM',
+        info: 'first redux action'
+    }
+}
+
+const initialCakeState = {
     numberOfCakes: 10
 }
 
 //reducer is a function that takes the current state and action as arguments and return a new state
-const reducer = (state = initialState, action) => {
+const cakeReducer = (state = initialCakeState, action) => {
     switch (action.type) {
         // Reducer donot allow to mutate the original so spread operator  is used to make copies of original and then mutate the copies
         case BUY_CAKE: return {
@@ -29,15 +37,39 @@ const reducer = (state = initialState, action) => {
 
 }
 
+const initialIcecreamState = {
+    numOfIceCreams: 20
+  }
+  
+  const iceCreamReducer = (state = initialIcecreamState, action) => {
+    switch (action.type) {
+      case BUY_ICECREAM: return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams - 1
+      }
+  
+      default: return state
+    }
+  }
+
 //getState() returnd current state
 //dispatch sends an action to reducer to update the state
 // subscribe takes a listener callback thhat runs each time an action is dispatched
 
-const store = createStore(reducer)
+const rootReducer = combineReducers({
+    cake: cakeReducer,
+    iceCream: iceCreamReducer
+
+})
+const store = createStore(rootReducer)
 console.log('Initial State', store.getState())
 const unsubscribe = store.subscribe(() => console.log('Update state', store.getState()))
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
+
 unsubscribe()
 
