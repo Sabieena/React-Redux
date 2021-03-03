8b7a60d5aeeafa8cd8c753bda1ed67327a842ec0
Sabieena <sabina.bhattrai2015@gmail.com>
2021-03-03 10:44:37 +0545

    added some contencts

--- index.js
+++ index.js
@@ -16,8 +16,10 @@ const initialState = {
     numberOfCakes: 10
 }
 
+//reducer is a function that takes the current state and action as arguments and return a new state
 const reducer = (state = initialState, action) => {
     switch (action.type) {
+        // Reducer donot allow to mutate the original so spread operator  is used to make copies of original and then mutate the copies
         case BUY_CAKE: return {
             ...state,
             numberOfCakes: state.numberOfCakes - 1
@@ -27,6 +29,9 @@ const reducer = (state = initialState, action) => {
 
 }
 
+//getState() returnd current state
+//dispatch sends an action to reducer to update the state
+// subscribe takes a listener callback thhat runs each time an action is dispatched
 
 const store = createStore(reducer)
 console.log('Initial State', store.getState())
@@ -35,3 +40,4 @@ store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 unsubscribe()
+
